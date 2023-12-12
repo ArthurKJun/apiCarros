95fca8ad71eb2a830564a4fefee8c45d09aa3604
ArthurKJun <149848250+ArthurKJun@users.noreply.github.com>
2023-12-11 22:41:56 -0300

    feito metodo get no controller e criado import.sql e banco dados

--- src/main/java/com/example/apiCarros/controller/CarroController.java
+++ src/main/java/com/example/apiCarros/controller/CarroController.java
@@ -1,9 +1,12 @@
 package com.example.apiCarros.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +29,8 @@ public class CarroController{
 		return ResponseEntity.status(HttpStatus.CREATED).body(ct);
 	}
 	
+	@GetMapping() 
+	public ResponseEntity<List<Carro>> getContatos() {
+		return ResponseEntity.status(HttpStatus.OK).body(repo.findAll());
+	}
 }
