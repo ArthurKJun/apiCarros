2f88990f8296ed4e8719430a8cdc65c2b7fa50c4
ArthurKJun <149848250+ArthurKJun@users.noreply.github.com>
2023-12-11 21:50:37 -0300

    projeto criado e packages entidade, repositorio e controller

---
+++ src/main/java/com/example/apiCarros/entidades/Carro.java
@@ -0,0 +1,17 @@
+package com.example.apiCarros.entidades;
+
+public class Carro{
+	
+	private long id;
+	private String dono;	
+	private String marca;
+	private String modelo;
+	private int anoFab;
+	private int anoMod;
+	private String Placa;
+	private String chassi;
+	private String renavam;
+	
+	
+	
+}
